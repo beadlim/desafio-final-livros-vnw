052ba6529f74d3f3e7021bf028afb61088bbbcb9
beadlim <beatrizdlim53@gmail.com>
2025-04-02 22:44:01 -0300

    feat: atualizar o get e post

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,20 +1,43 @@
 import S from './doados.module.scss'
 import livro from '../../assets/livro.png'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
+export default function Doados() {
+    const [livros, setLivros] = useState([])
 
-export default function Doados(){
-    return(
+    const getLivros = async () => {
+        try {
+            const response = await axios.get("https://desafio2-python.onrender.com/livros")
+            setLivros(response.data)
+        } catch (error) {
+            console.error("Erro ao buscar livros:", error)
+        }
+    }
+
+    useEffect(() => {
+        getLivros()
+    }, [])
+
+    return (
         <section className={S.boxDoados}>
             <h2>Livros Doados</h2>
-            <section className={S.boxCard}>
+            <section className={S.boxBooks}>
                 <article>
-                    <img src={livro} alt="imagem de um livro que se chama o protagonista" />
-                    
+                    <img src={livro} alt="imagem do livro o protagonista" />
                     <h3>O Protagonista</h3>
                     <p>Susanne Andrade</p>
                     <p>Ficção</p>
-                    
                 </article>
+                
+                {livros.map((item) => (
+                    <article key={item.id}>
+                        <img src={item.imagem_url} alt={item.titulo} />
+                        <h3>{item.titulo}</h3>
+                        <p>{item.autor}</p>
+                        <p>{item.categoria}</p>
+                    </article>
+                ))}
             </section>
         </section>
     )
